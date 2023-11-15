285109830291aab81abe8f6f71f89701e1b8c836
khattab88 <m_khatab_88@hotmail.com>
2023-11-15 20:35:07 +0200

    implement worker service

--- WebSiteStatus/Worker.cs
+++ WebSiteStatus/Worker.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,17 +12,40 @@ namespace WebSiteStatus
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HttpClient _httpClient;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _httpClient = new HttpClient();
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _httpClient.Dispose();
+
+            return base.StopAsync(cancellationToken);
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                var result = await _httpClient.GetAsync("https://www.iamtimcorey.com");
+                if (result.IsSuccessStatusCode) 
+                {
+                    _logger.LogInformation($"Website is up, Status code {result.StatusCode.ToString()} at: {DateTimeOffset.Now}");
+                }
+                else
+                {
+                    _logger.LogError($"Website is down, Status code {result.StatusCode.ToString()} at: {DateTimeOffset.Now}");
+                }
 
                 await Task.Delay(5*1000, stoppingToken);
             }
